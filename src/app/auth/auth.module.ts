5d9a63e57fee7b2f8c1ee9549594587dbfe6ea88
dav24github <davidthbsossa@gmail.com>
2021-09-13 21:36:32 -0400

    another commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -11,7 +12,7 @@ const routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
